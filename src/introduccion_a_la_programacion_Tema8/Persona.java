26885593422c4de18a6b5896e750a5b3983f4159
Duarte Andrés <duarteandresj@gmail.com>
2022-11-14 16:43:56 -0300

    Se agrega ejercicio Tema8

---
+++ src/introduccion_a_la_programacion_Tema8/Persona.java
@@ -0,0 +1,43 @@
+package introduccion_a_la_programacion_Tema8;
+
+public class Persona {
+	private int edad;
+	private String nombre;
+	private String telefono;
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public static void main(String[] args) {
+		Persona nuevaPersona = new Persona();
+
+		nuevaPersona.setEdad(25);
+		nuevaPersona.setNombre("Pepito");
+		nuevaPersona.setTelefono("+5491112345678");
+
+		System.out.println("Edad: " + nuevaPersona.getEdad());
+		System.out.println("Nombre: " + nuevaPersona.getNombre());
+		System.out.println("Telefono: " + nuevaPersona.getTelefono());
+	}
+}
